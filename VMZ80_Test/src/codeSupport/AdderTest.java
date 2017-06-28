ad3540b815fb61509acb015edd196801f64ff1a4
Frank Martyn <FrankMartyn1945@gmail.com>
2017-06-28 17:27:05 -0400

    All obvious Adder methods have been coded and tested

--- VMZ80_Test/src/codeSupport/AdderTest.java
+++ VMZ80_Test/src/codeSupport/AdderTest.java
@@ -17,44 +17,17 @@ public class AdderTest {
 	byte[] word1 = new byte[] { (byte) 00, (byte) 00 };
 	byte[] word2 = new byte[] { (byte) 00, (byte) 00 };
 	byte[] answers;
-	// BitSet ans1;
-	// BitSet ans2;
 	int val1, val2, val3, val4, ansByte, ansWord;
 
 	@Before
 	public void setUp() throws Exception {
 		assertThat("keep imports", 1, equalTo(1));
 		adder = Adder.getInstance();
-		adder.clearSets();
+//		adder.clearSets();
 	}// setUp
 
-	// @Test
-	// public void simpleTestOfAdd() {
-	// int sum1, sum2;
-	// for (val1 = 0; val1 < 0X100; val1++) {
-	// for (val2 = 0; val2 < 0X100; val2++) {
-	// sum1 = val1 + val2;
-	// sum2 = (sum1 & 0X100) >> 8;
-	// if (sum2 == 0) {
-	// answers = new byte[] { (byte) (sum1 & 0XFF) };
-	// } else {
-	// answers = new byte[] { (byte) (sum1 & 0XFF), (byte) sum2 };
-	// }
-	// bite1[0] = (byte) val1;
-	// bite2[0] = (byte) val2;
-	// adder.setArguments(bite1, bite2);
-	// adder.add();
-	// byte[] ans = adder.getSum();
-	// assertThat("Simple Add: " + val1 + " " + val2, answers, equalTo(adder.getSum()));
-	//// showAnswer(val1, val2, ans);
-	// } // for val2
-	// } // for val1
-	// }// simpleTestOfAdd
-
 	@Test
-	public void testOfByteAdd() {
-		
-		int sum1;
+	public void testByteAdd() {
 		for (val1 = 0; val1 < 0X100; val1++) {
 			for (val2 = 0; val2 < 0X100; val2++) {
 				answers = new byte[] { (byte) ((val1 + val2) & 0XFF) };
@@ -67,28 +40,296 @@ public class AdderTest {
 
 	@Test
 	public void testWordAdd() {
-		int total,result;
+		int total, result;
 		Random random = new Random();
-		int COUNT = 3000;
-		for( int i= 0;i< COUNT; i++){
+		int COUNT = 1;
+		for (int i = 0; i < COUNT; i++) {
 			val1 = random.nextInt(0XFFFF);
-//			val1 = 0XFF00;
-			word1[0] = (byte)( val1 &0XFF);
-			word1[1] = (byte)(( val1 &0XFF00)>>8);
+			word1 = loadWord(val1);
 			val2 = random.nextInt(0XFFFF);
-//			val2 = 0XFF;
-			word2[0] = (byte)( val2 &0XFF);
-			word2[1] = (byte)(( val2 &0XFF00)>>8);
-//			System.out.printf("val1 = %04X, word1[1] = %02X, word1[0] = %02X%n", val1, word1[1], word1[0]);
-//			System.out.printf("val2 = %04X, word2[1] = %02X, word2[0] = %02X%n", val2, word2[1], word2[0]);
+			word2 = loadWord(val2);
+			// System.out.printf("val1 = %04X, word1[1] = %02X, word1[0] = %02X%n", val1, word1[1], word1[0]);
+			// System.out.printf("val2 = %04X, word2[1] = %02X, word2[0] = %02X%n", val2, word2[1], word2[0]);
 			total = (val1 + val2) & 0XFFFF;
 			answers = adder.add(word1, word2);
 			result = ((answers[1] * 0X100) + (answers[0] & 0XFF)) & 0XFFFF;
-//			System.out.printf("total = %04X,  result = %04X%n", total,result);
-			assertThat("Word Add (" + i + ") " + val1 + " + " + val2,total,equalTo(result));
-		}//for
-		
+			// System.out.printf("total = %04X, result = %04X%n", total,result);
+			assertThat("Word Add (" + i + ") " + val1 + " + " + val2, total, equalTo(result));
+		} // for
 	}// testWordAdd
+
+	@Test
+	public void testZFlag() {
+		Random random = new Random();
+		int COUNT = 3000;
+		boolean zeroFlag;
+		for (int i = 0; i < COUNT; i++) {
+			val1 = random.nextInt(0XFF);
+			val2 = random.nextInt(0XFF);
+			if (random.nextInt(10) == 1) {
+				val2 = -val1;
+			} // if
+			zeroFlag = (((val1 + val2) & 0XFF) == 0) ? true : false;
+			bite1[0] = (byte) val1;
+			bite2[0] = (byte) val2;
+			adder.add(bite1, bite2);
+			assertThat("testZFlag byte:  " + val1 + " + " + val2 + " = " + (val2 + val1), zeroFlag,
+					equalTo(adder.isZero()));
+		} // for
+
+		for (int i = 0; i < COUNT; i++) {
+			val1 = random.nextInt(0XFFFF);
+			val2 = random.nextInt(0XFFFF);
+			if (random.nextInt(10) == 1) {
+				val2 = -val1;
+			} // if
+			zeroFlag = (((val1 + val2) & 0XFFFF) == 0) ? true : false;
+			word1 = loadWord(val1);
+			word2 = loadWord(val2);
+			adder.add(word1, word2);
+			assertThat("testZFlag word:  " + val1 + " + " + val2 + " = " + (val2 + val1), zeroFlag,
+					equalTo(adder.isZeroWord()));
+		} // for
+
+	}// testZFlag
+	
+	@Test
+	public void testSign() {
+		Random random = new Random();
+		int COUNT = 3000;
+		boolean signFlag;
+		for (int i = 0; i < COUNT; i++) {
+			val1 = random.nextInt(0XFF);
+			val2 = random.nextInt(0XFF);
+
+			signFlag = (((val1 + val2) & 0X80) == 0X80) ? true : false;
+			bite1[0] = (byte) val1;
+			bite2[0] = (byte) val2;
+			adder.add(bite1, bite2);
+			assertThat("testSign byte:  " + val1 + " + " + val2 + " = " + (val2 + val1), signFlag,
+					equalTo(adder.hasSign()));
+		} // for
+
+		for (int i = 0; i < COUNT; i++) {
+			val1 = random.nextInt(0XFFFF);
+			val2 = random.nextInt(0XFFFF);
+			signFlag = (((val1 + val2) & 0X8000) == 0X8000) ? true : false;
+			word1 = loadWord(val1);
+			word2 = loadWord(val2);
+			adder.add(word1, word2);
+			assertThat("testSign word:  " + val1 + " + " + val2 + " = " + (val2 + val1), signFlag,
+					equalTo(adder.hasSignWord()));
+		} // for
+	}// testSign
+
+	@Test
+	public void testHalfCarry() {
+		
+		Random random = new Random();
+		int COUNT = 3000;
+		boolean halfCarry;
+		int nibbleMask = 0X0F;
+		for (int i = 0; i < COUNT; i++) {
+			val1 = random.nextInt(0XFF);
+			val2 = random.nextInt(0XFF);
+			halfCarry = ((val1 & nibbleMask) + ( val2 & nibbleMask)) > nibbleMask ? true : false;		
+			bite1[0] = (byte) val1;
+			bite2[0] = (byte) val2;
+			adder.add(bite1, bite2);
+			assertThat("testHalfCarry byte:  " + val1 + " + " + val2 + " = " + (val2 + val1), halfCarry,
+					equalTo(adder.hasHalfCarry()));
+		} // for
+		
+		
+		int byteAndNibbleMask = 0X0FFF;
+		for (int i = 0; i < COUNT; i++) {
+			val1 = random.nextInt(0XFFFF);
+			val2 = random.nextInt(0XFFFF);
+			halfCarry = ((val1 & byteAndNibbleMask) + ( val2 & byteAndNibbleMask)) > byteAndNibbleMask ? true : false;
+			word1 = loadWord(val1);
+			word2 = loadWord(val2);
+			adder.add(word1, word2);
+			assertThat("testHalfCarry word:  " + val1 + " + " + val2 + " = " + (val2 + val1), halfCarry,
+					equalTo(adder.hasHalfCarryWord()));
+		} // for
+		
+	}// testHalfCarry
+	
+	@Test
+	public void testParity() {	
+		Random random = new Random();
+		int COUNT = 3000;
+		boolean parityFlag;
+		String s;
+		for (int i = 0; i < COUNT; i++) {
+			val1 = random.nextInt(0XFF);
+			val2 = random.nextInt(0XFF);
+			s = Integer.toBinaryString((val1 + val2) & 0XFF);
+			s = s.replaceAll("0", "");
+			parityFlag = (s.length() % 2)== 0 ? true : false;		
+			bite1[0] = (byte) val1;
+			bite2[0] = (byte) val2;
+			adder.add(bite1, bite2);
+			assertThat("testParity byte:  " + val1 + " + " + val2 + " = " + (val2 + val1), parityFlag,
+					equalTo(adder.hasParity()));
+		} // for
+		
+		for (int i = 0; i < COUNT; i++) {
+			val1 = random.nextInt(0XFFFF);
+			val2 = random.nextInt(0XFFFF);
+			s = Integer.toBinaryString((val1 + val2) & 0XFFFF);
+			s = s.replaceAll("0", "");
+			parityFlag = (s.length() % 2)== 0 ? true : false;		
+
+			word1 = loadWord(val1);
+			word2 = loadWord(val2);
+			adder.add(word1, word2);
+			assertThat("testParity word:  " + val1 + " + " + val2 + " = " + (val2 + val1), parityFlag,
+					equalTo(adder.hasParityWord()));
+		} // for
+	}// testParity
+	
+	@Test
+	public void testOverflow() {	
+		Random random = new Random();
+		int COUNT = 3000;
+		boolean overFlow;
+		
+		for (int i = 0; i < COUNT; i++) {
+			val1 = random.nextInt(0XFF);
+			val2 = random.nextInt(0XFF);
+			int ans = (val1 + val2) & 0X0FF;
+			
+			boolean sign1 = (val1 & 0X80) == 0X080;
+			boolean sign2 = (val2 & 0X80) == 0X080;
+			boolean signAns = (ans & 0X80) == 0X080;
+			boolean overflow = false;
+			if (!(sign1 ^ sign2)) {
+				overflow = sign1 ^ signAns;
+			} // if
+//			System.out.printf("overflow = %s,sign1 = %s,sign2 = %s,signAns = %s%n",overflow,sign1,sign2,signAns);
+				
+			bite1[0] = (byte) val1;
+			bite2[0] = (byte) val2;
+			adder.add(bite1, bite2);
+			assertThat("testOverflow byte:  " + val1 + " + " + val2 + " = " + (val2 + val1), overflow,
+					equalTo(adder.hasOverflow()));
+		} // for
+		
+		for (int i = 0; i < COUNT; i++) {
+			val1 = random.nextInt(0XFFFF);
+			val2 = random.nextInt(0XFFFF);
+			int ans = (val1 + val2) & 0X0FFFF;
+
+			boolean sign1 = (val1 & 0X8000) == 0X8000;
+			boolean sign2 = (val2 & 0X8000) == 0X8000;
+			boolean signAns = (ans & 0X8000) == 0X8000;
+
+			boolean overflow = false;
+			if (!(sign1 ^ sign2)) {
+				overflow = sign1 ^ signAns;
+			} // if
+
+			word1 = loadWord(val1);
+			word2 = loadWord(val2);
+			adder.add(word1, word2);
+			assertThat("testOverflow word:  " + val1 + " + " + val2 + " = " + (val2 + val1), overflow,
+					equalTo(adder.hasOverflowWord()));
+		} // for
+	}// testOverflow
+	
+	@Test
+	public void testCarry() {
+		
+		Random random = new Random();
+		int COUNT = 3000;
+		boolean carry;
+		int byteMask = 0XFF;
+		for (int i = 0; i < COUNT; i++) {
+			val1 = random.nextInt(0XFF);
+			val2 = random.nextInt(0XFF);
+			carry = ((val1 & byteMask) + ( val2 & byteMask)) > byteMask ? true : false;		
+			bite1[0] = (byte) val1;
+			bite2[0] = (byte) val2;
+			adder.add(bite1, bite2);
+			assertThat("testCarry byte:  " + val1 + " + " + val2 + " = " + (val2 + val1), carry,
+					equalTo(adder.hasCarry()));
+		} // for
+		
+		
+		int wordMask = 0X0FFFF;
+		for (int i = 0; i < COUNT; i++) {
+			val1 = random.nextInt(0XFFFF);
+			val2 = random.nextInt(0XFFFF);
+			carry = ((val1 & wordMask) + ( val2 & wordMask)) > wordMask ? true : false;
+			word1 = loadWord(val1);
+			word2 = loadWord(val2);
+			adder.add(word1, word2);
+			assertThat("testCarry word:  " + val1 + " + " + val2 + " = " + (val2 + val1), carry,
+					equalTo(adder.hasCarryWord()));
+		} // for
+		
+	}// testHalfCarry
+	
+	@Test
+	public void testAND() {
+		
+		for (val1 = 0; val1 < 0X100; val1++) {
+			for (val2 = 0; val2 < 0X100; val2++) {
+				answers = new byte[] { (byte) ((val1 & val2) & 0XFF) };
+				bite1[0] = (byte) val1;
+				bite2[0] = (byte) val2;
+				assertThat("Full AND:  " + val1 + " " + val2, answers[0], equalTo(adder.and(bite1, bite2)[0]));
+			} // for val2
+		} // for val1
+		
+		
+	}// testAND
+	
+	@Test
+	public void testOR() {
+		
+		for (val1 = 0; val1 < 0X100; val1++) {
+			for (val2 = 0; val2 < 0X100; val2++) {
+				answers = new byte[] { (byte) ((val1 | val2) & 0XFF) };
+				bite1[0] = (byte) val1;
+				bite2[0] = (byte) val2;
+				assertThat("Full OR:  " + val1 + " " + val2, answers[0], equalTo(adder.or(bite1, bite2)[0]));
+			} // for val2
+		} // for val1
+		
+		
+	}// testAND
+	
+	@Test
+	public void testXOR() {
+		
+		for (val1 = 0; val1 < 0X100; val1++) {
+			for (val2 = 0; val2 < 0X100; val2++) {
+				answers = new byte[] { (byte) ((val1 ^ val2) & 0XFF) };
+				bite1[0] = (byte) val1;
+				bite2[0] = (byte) val2;
+				assertThat("Full XOR:  " + val1 + " " + val2, answers[0], equalTo(adder.xor(bite1, bite2)[0]));
+			} // for val2
+		} // for val1
+		
+		
+	}// testAND
+	
+	@Test
+	public void testCPL() {	
+		for (val1 = 0; val1 < 0X100; val1++) {	
+				answers = new byte[] { (byte) (~val1 & 0XFF) };
+				bite1[0] = (byte) val1;
+				assertThat("Full CPL:  " + val1 , answers[0], equalTo(adder.complement(bite1)[0]));
+		} // for val1
+	}// testCPL
+	
+//---------------------------------------------------------------------------------------
+	// returns LSB in index 0 and MSB in index 1
+	private byte[] loadWord(int value) {
+		return new byte[] { (byte) (value & 0XFF), (byte) ((value & 0XFF00) >> 8) };
+	}// loadWord
 
 	private void showAnswer(int val1, int val2, byte[] ans) {
 		for (int i = 0; i < ans.length; i++) {
---
+++ VMZ80_Test/src/codeSupport/AdderTestLite.java
@@ -0,0 +1,102 @@
+package codeSupport;
+
+import hardware.Adder;
+
+public class AdderTestLite {
+	static Adder adder = Adder.getInstance();
+
+	public static void main(String[] args) {
+		checkNOT();
+		checkAnd();
+		checkOverFlow();
+		checkParity();
+		addWord();
+		addByte();
+	}// main
+	
+	private static void checkNOT(){
+		byte b = (byte) 0XA5;
+		byte bb = (byte)~b;
+		System.out.printf("b = %s,\tbb= %s%n",Integer.toBinaryString(b),Integer.toBinaryString(bb));
+	}
+	
+	private static void checkAnd(){
+		int val1 = 0X7A;
+		int val2 = 0XAA;
+		byte[] bite1 = new byte[]{(byte) val1};
+		byte[] bite2 = new byte[]{(byte) val2};
+		byte[] ans = adder.and(bite1, bite2);
+		
+		System.out.printf("parity = %s,\tans[0] = %02X,\t val1 = %02X,\t val2 = %02X%n",adder.hasParity(), ans[0],val1,val2);
+	}
+
+	private static void checkOverFlow() {
+		int val1 = 0XBF;
+		int val2 = 0X95;
+
+		int ans = (val1 + val2) & 0X0FF;
+
+		boolean sign1 = (val1 & 0X80) == 0X080;
+		boolean sign2 = (val2 & 0X80) == 0X080;
+		boolean signAns = (ans & 0X80) == 0X080;
+		boolean overflow = false;
+		if (!(sign1 ^ sign2)) {
+			overflow = sign1 ^ signAns;
+		} // if		
+		System.out.printf("overflow = %s,\tsign1 = %s,\tsign2 = %s\t,signAns = %s%n",overflow,sign1,sign2,signAns);
+		
+		int maskIn = 0X7F;
+		int maskOut = 0XFF;
+		boolean carryIn = ((val1 & maskIn)+ (val2 & maskIn) >maskIn);
+		boolean carryOut = ((val1 & maskOut)+ (val2 & maskOut) >maskOut);
+		overflow = carryIn ^ carryOut;
+		System.out.printf("overflow = %s,\tcarryIn = %s,\tcarryOut = %s%n",overflow,carryIn,carryOut);
+		
+		byte[] bite1 = new byte[]{(byte) val1};
+		byte[] bite2 = new byte[]{(byte) val2};
+		
+		adder.add(bite1, bite2);
+		System.out.printf("adder.hasOverflow() = %s%n",adder.hasOverflow());
+
+	}// checkOverFlow
+
+	private static void checkParity() {
+		int val1 = 7473;
+		int val2 = 42214;
+		String s = Integer.toBinaryString((val1 + val2) & 0XFFFF);
+		s = s.replaceAll("0", "");
+		boolean parityFlag = (s.length() % 2) == 0 ? true : false;
+
+		byte[] word1 = loadWord(val1);
+		byte[] word2 = loadWord(val2);
+		adder.add(word1, word2);
+		boolean pf = adder.hasParityWord();
+		if (pf == parityFlag) {
+			int a = 1;
+		}
+	}// countBits
+
+	private static void addWord() {
+		int val1 = 3096;
+		int val2 = -val1;
+		byte[] word1 = loadWord(val1);
+		byte[] word2 = loadWord(val2);
+		adder.add(word1, word2);
+		boolean zeroFlag = (((val1 + val2) & 0XFFFF) == 0) ? true : false;
+		boolean zflag = adder.isZero();
+		boolean x = true;
+	}// addWord
+
+	private static void addByte() {
+		byte[] bite1 = new byte[] { (byte) 0X4D };
+		byte[] bite2 = new byte[] { (byte) 0X33 };
+
+		adder.add(bite1, bite2);
+		boolean zflag = adder.isZero();
+	}// addByte
+
+	private static byte[] loadWord(int value) {
+		return new byte[] { (byte) (value & 0XFF), (byte) ((value & 0XFF00) >> 8) };
+	}// loadWord
+
+}// AdderTestLite
