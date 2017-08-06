2d33b613c0bc235e7c9e76cb08ed2c1bb7367279
Frank Martyn <FrankMartyn1945@gmail.com>
2017-08-06 14:43:46 -0400

    need to fix line 457 in ADDER

--- VMZ80_Test/src/hardware/AdderAllTests.java
+++ VMZ80_Test/src/hardware/AdderAllTests.java
@@ -7,7 +7,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ AdderAllRotates.class,
 	AdderAllStandardTests.class,
-	AdderBits.class })
+	AdderBits.class,
+	AdderDAATest.class})
 public class AdderAllTests {
 
 }
