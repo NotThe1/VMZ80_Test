7f0821be1eb7f82198355bf51e711ab349867dc1
Frank Martyn <FrankMartyn1945@gmail.com>
2017-08-13 14:59:16 -0400

    BYTE ADD ADC and INC test code tested. The file data for INC is complete, the ADD and ADC data needs to be produced completely

--- VMZ80_Test/src/hardware/AdderByteAdd.java
+++ VMZ80_Test/src/hardware/AdderByteAdd.java
@@ -2,6 +2,10 @@ package hardware;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.io.InputStream;
+import java.util.Scanner;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -31,156 +35,156 @@ public class AdderByteAdd {
 	}// setUp
 	
 	
-//	@Test
-//	public void testADDfile() {
-//		
-//		try {
-//			InputStream inputStream = this.getClass().getResourceAsStream("/AddOriginal.txt");
-////			InputStream inputStream = this.getClass().getResourceAsStream("/daaTemp.txt");
-//			Scanner scanner = new Scanner(inputStream);
-//			scanner.nextLine(); // skip header
-//			while (scanner.hasNextLine()){
-//				arg1 = getValue(scanner.next());
-//				arg2 = getValue(scanner.next());
-//				sum = getValue(scanner.next());
-//				flags = scanner.next();
-//				
-//				sign = flags.subSequence(0, 1).equals("1")?true:false;
-//				zero = flags.subSequence(1, 2).equals("1")?true:false;
-//				halfCarry = flags.subSequence(2, 3).equals("1")?true:false;
-//				overflow = flags.subSequence(3, 4).equals("1")?true:false;
-//				nFlag = flags.subSequence(4, 5).equals("1")?true:false;
-//				carry = flags.subSequence(5, 6).equals("1")?true:false;
-//
-////				System.out.printf("%02X %02X %02X %s ",arg1,arg2,sum,flags);
-////				System.out.printf("  %s %s %s   %s %s %s %n", sign,zero,halfCarry,overflow,nFlag,carry);
-//						
-//				message = String.format("file ADD -> %d - %d = %02X", arg1,arg2,sum);
-//				assertThat("ans: " + message,sum,equalTo(adder.add(arg1, arg2)));
-//				assertThat("sign: " +  message,sign,equalTo(adder.hasSign()));
-//				assertThat("zero: " +  message,zero,equalTo(adder.isZero()));
-//				assertThat("halfCarry: " +  message,halfCarry,equalTo(adder.hasHalfCarry()));
+	@Test
+	public void testADDfile() {
+		
+		try {
+			InputStream inputStream = this.getClass().getResourceAsStream("/AddOriginal.txt");
+//			InputStream inputStream = this.getClass().getResourceAsStream("/daaTemp.txt");
+			Scanner scanner = new Scanner(inputStream);
+			scanner.nextLine(); // skip header
+			while (scanner.hasNextLine()){
+				arg1 = getValue(scanner.next());
+				arg2 = getValue(scanner.next());
+				sum = getValue(scanner.next());
+				flags = scanner.next();
+				
+				sign = flags.subSequence(0, 1).equals("1")?true:false;
+				zero = flags.subSequence(1, 2).equals("1")?true:false;
+				halfCarry = flags.subSequence(2, 3).equals("1")?true:false;
+				overflow = flags.subSequence(3, 4).equals("1")?true:false;
+				nFlag = flags.subSequence(4, 5).equals("1")?true:false;
+				carry = flags.subSequence(5, 6).equals("1")?true:false;
+
+//				System.out.printf("%02X %02X %02X %s ",arg1,arg2,sum,flags);
+//				System.out.printf("  %s %s %s   %s %s %s %n", sign,zero,halfCarry,overflow,nFlag,carry);
+						
+				message = String.format("file ADD -> %d - %d = %02X", arg1,arg2,sum);
+				assertThat("ans: " + message,sum,equalTo(adder.add(arg1, arg2)));
+				assertThat("sign: " +  message,sign,equalTo(adder.hasSign()));
+				assertThat("zero: " +  message,zero,equalTo(adder.isZero()));
+				assertThat("halfCarry: " +  message,halfCarry,equalTo(adder.hasHalfCarry()));
+				assertThat("overFlow: " +  message,overflow,equalTo(adder.hasOverflow()));
+				assertThat("nFlag: " +  message,nFlag,equalTo(adder.isNFlagSet()));
+				assertThat("carry: " +  message,carry,equalTo(adder.hasCarry()));			
+			}//while
+			scanner.close();
+			inputStream.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			fail(e.getMessage());
+		}//try
+	}//testADDfile
+	
+	@Test
+	public void testADCfile() {
+		
+		try {
+			InputStream inputStream = this.getClass().getResourceAsStream("/AdcOriginal.txt");
+//			InputStream inputStream = this.getClass().getResourceAsStream("/daaTemp.txt");
+			Scanner scanner = new Scanner(inputStream);
+			scanner.nextLine(); // skip header
+			while (scanner.hasNextLine()){
+				arg1 = getValue(scanner.next());
+				arg2 = getValue(scanner.next());
+				sum = getValue(scanner.next());
+				flags = scanner.next();
+				
+				carryState = false;
+				
+				sign = flags.subSequence(0, 1).equals("1")?true:false;
+				zero = flags.subSequence(1, 2).equals("1")?true:false;
+				halfCarry = flags.subSequence(2, 3).equals("1")?true:false;
+				overflow = flags.subSequence(3, 4).equals("1")?true:false;
+				nFlag = flags.subSequence(4, 5).equals("1")?true:false;
+				carry = flags.subSequence(5, 6).equals("1")?true:false;
+
+//				System.out.printf("%02X %02X %02X %s ",arg1,arg2,sum,flags);
+//				System.out.printf("  %s %s %s   %s %s %s %n", sign,zero,halfCarry,overflow,nFlag,carry);
+						
+				message = String.format("file ADC NC -> %d - %d = %02X", arg1,arg2,sum);
+				assertThat("ans: " + message,sum,equalTo(adder.addWithCarry(arg1, arg2,carryState)));
+				assertThat("sign: " +  message,sign,equalTo(adder.hasSign()));
+				assertThat("zero: " +  message,zero,equalTo(adder.isZero()));
+				assertThat("halfCarry: " +  message,halfCarry,equalTo(adder.hasHalfCarry()));
+				assertThat("overFlow: " +  message,overflow,equalTo(adder.hasOverflow()));
+				assertThat("nFlag: " +  message,nFlag,equalTo(adder.isNFlagSet()));
+				assertThat("carry: " +  message,carry,equalTo(adder.hasCarry()));
+				
+				//---------------------------------------------------------------------------------
+				
+				sum = getValue(scanner.next());
+				flags = scanner.next();
+				
+				carryState = true;
+				
+				sign = flags.subSequence(0, 1).equals("1")?true:false;
+				zero = flags.subSequence(1, 2).equals("1")?true:false;
+				halfCarry = flags.subSequence(2, 3).equals("1")?true:false;
+				overflow = flags.subSequence(3, 4).equals("1")?true:false;
+				nFlag = flags.subSequence(4, 5).equals("1")?true:false;
+				carry = flags.subSequence(5, 6).equals("1")?true:false;
+
+//				System.out.printf("%02X %02X %02X %s ",arg1,arg2,sum,flags);
+//				System.out.printf("  %s %s %s   %s %s %s %n", sign,zero,halfCarry,overflow,nFlag,carry);
+						
+				message = String.format("file ADC CY -> %d - %d = %02X", arg1,arg2,sum);
+				assertThat("ans: " + message,sum,equalTo(adder.addWithCarry(arg1, arg2,carryState)));
+				assertThat("sign: " +  message,sign,equalTo(adder.hasSign()));
+				assertThat("zero: " +  message,zero,equalTo(adder.isZero()));
+				assertThat("halfCarry: " +  message,halfCarry,equalTo(adder.hasHalfCarry()));
+				assertThat("overFlow: " +  message,overflow,equalTo(adder.hasOverflow()));
+				assertThat("nFlag: " +  message,nFlag,equalTo(adder.isNFlagSet()));
+				assertThat("carry: " +  message,carry,equalTo(adder.hasCarry()));
+			}//while
+			scanner.close();
+			inputStream.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			fail(e.getMessage());
+		}//try
+	}//testADCfile
+	
+	
+	@Test
+	public void testINCfile() {
+		
+		try {
+			InputStream inputStream = this.getClass().getResourceAsStream("/IncOriginal.txt");
+//			InputStream inputStream = this.getClass().getResourceAsStream("/daaTemp.txt");
+			Scanner scanner = new Scanner(inputStream);
+			scanner.nextLine(); // skip header
+			while (scanner.hasNextLine()){
+				arg1 = getValue(scanner.next());
+				sum = getValue(scanner.next());
+				flags = scanner.next();
+				
+				sign = flags.subSequence(0, 1).equals("1")?true:false;
+				zero = flags.subSequence(1, 2).equals("1")?true:false;
+				halfCarry = flags.subSequence(2, 3).equals("1")?true:false;
+				overflow = flags.subSequence(3, 4).equals("1")?true:false;
+				nFlag = flags.subSequence(4, 5).equals("1")?true:false;
+				carry = flags.subSequence(5, 6).equals("1")?true:false;
+
+//				System.out.printf("%02X  %02X %s ",arg1,sum,flags);
+//				System.out.printf("  %s %s %s   %s %s %s %n", sign,zero,halfCarry,overflow,nFlag,carry);
+						
+				message = String.format("file INC -> %d  = %02X", arg1,sum);
+				assertThat("ans: " + message,sum,equalTo(adder.increment(arg1)));
+				assertThat("sign: " +  message,sign,equalTo(adder.hasSign()));
+				assertThat("zero: " +  message,zero,equalTo(adder.isZero()));
+				assertThat("halfCarry: " +  message,halfCarry,equalTo(adder.hasHalfCarry()));
 //				assertThat("overFlow: " +  message,overflow,equalTo(adder.hasOverflow()));
-//				assertThat("nFlag: " +  message,nFlag,equalTo(adder.isNFlagSet()));
+				assertThat("nFlag: " +  message,nFlag,equalTo(adder.isNFlagSet()));
 //				assertThat("carry: " +  message,carry,equalTo(adder.hasCarry()));			
-//			}//while
-//			scanner.close();
-//			inputStream.close();
-//		} catch (Exception e) {
-//			System.out.println(e.getMessage());
-//			fail(e.getMessage());
-//		}//try
-//	}//testADDfile
-	
-//	@Test
-//	public void testADCfile() {
-//		
-//		try {
-//			InputStream inputStream = this.getClass().getResourceAsStream("/AdcOriginal.txt");
-////			InputStream inputStream = this.getClass().getResourceAsStream("/daaTemp.txt");
-//			Scanner scanner = new Scanner(inputStream);
-//			scanner.nextLine(); // skip header
-//			while (scanner.hasNextLine()){
-//				arg1 = getValue(scanner.next());
-//				arg2 = getValue(scanner.next());
-//				sum = getValue(scanner.next());
-//				flags = scanner.next();
-//				
-//				carryState = false;
-//				
-//				sign = flags.subSequence(0, 1).equals("1")?true:false;
-//				zero = flags.subSequence(1, 2).equals("1")?true:false;
-//				halfCarry = flags.subSequence(2, 3).equals("1")?true:false;
-//				overflow = flags.subSequence(3, 4).equals("1")?true:false;
-//				nFlag = flags.subSequence(4, 5).equals("1")?true:false;
-//				carry = flags.subSequence(5, 6).equals("1")?true:false;
-//
-////				System.out.printf("%02X %02X %02X %s ",arg1,arg2,sum,flags);
-////				System.out.printf("  %s %s %s   %s %s %s %n", sign,zero,halfCarry,overflow,nFlag,carry);
-//						
-//				message = String.format("file ADC NC -> %d - %d = %02X", arg1,arg2,sum);
-//				assertThat("ans: " + message,sum,equalTo(adder.addWithCarry(arg1, arg2,carryState)));
-//				assertThat("sign: " +  message,sign,equalTo(adder.hasSign()));
-//				assertThat("zero: " +  message,zero,equalTo(adder.isZero()));
-//				assertThat("halfCarry: " +  message,halfCarry,equalTo(adder.hasHalfCarry()));
-//				assertThat("overFlow: " +  message,overflow,equalTo(adder.hasOverflow()));
-//				assertThat("nFlag: " +  message,nFlag,equalTo(adder.isNFlagSet()));
-//				assertThat("carry: " +  message,carry,equalTo(adder.hasCarry()));
-//				
-//				//---------------------------------------------------------------------------------
-//				
-//				sum = getValue(scanner.next());
-//				flags = scanner.next();
-//				
-//				carryState = true;
-//				
-//				sign = flags.subSequence(0, 1).equals("1")?true:false;
-//				zero = flags.subSequence(1, 2).equals("1")?true:false;
-//				halfCarry = flags.subSequence(2, 3).equals("1")?true:false;
-//				overflow = flags.subSequence(3, 4).equals("1")?true:false;
-//				nFlag = flags.subSequence(4, 5).equals("1")?true:false;
-//				carry = flags.subSequence(5, 6).equals("1")?true:false;
-//
-////				System.out.printf("%02X %02X %02X %s ",arg1,arg2,sum,flags);
-////				System.out.printf("  %s %s %s   %s %s %s %n", sign,zero,halfCarry,overflow,nFlag,carry);
-//						
-//				message = String.format("file ADC CY -> %d - %d = %02X", arg1,arg2,sum);
-//				assertThat("ans: " + message,sum,equalTo(adder.addWithCarry(arg1, arg2,carryState)));
-//				assertThat("sign: " +  message,sign,equalTo(adder.hasSign()));
-//				assertThat("zero: " +  message,zero,equalTo(adder.isZero()));
-//				assertThat("halfCarry: " +  message,halfCarry,equalTo(adder.hasHalfCarry()));
-//				assertThat("overFlow: " +  message,overflow,equalTo(adder.hasOverflow()));
-//				assertThat("nFlag: " +  message,nFlag,equalTo(adder.isNFlagSet()));
-//				assertThat("carry: " +  message,carry,equalTo(adder.hasCarry()));
-//			}//while
-//			scanner.close();
-//			inputStream.close();
-//		} catch (Exception e) {
-//			System.out.println(e.getMessage());
-//			fail(e.getMessage());
-//		}//try
-//	}//testADCfile
-	
-	
-//	@Test
-//	public void testINCfile() {
-//		
-//		try {
-//			InputStream inputStream = this.getClass().getResourceAsStream("/IncOrignal.txt");
-////			InputStream inputStream = this.getClass().getResourceAsStream("/daaTemp.txt");
-//			Scanner scanner = new Scanner(inputStream);
-//			scanner.nextLine(); // skip header
-//			while (scanner.hasNextLine()){
-//				arg1 = getValue(scanner.next());
-//				sum = getValue(scanner.next());
-//				flags = scanner.next();
-//				
-//				sign = flags.subSequence(0, 1).equals("1")?true:false;
-//				zero = flags.subSequence(1, 2).equals("1")?true:false;
-//				halfCarry = flags.subSequence(2, 3).equals("1")?true:false;
-//				overflow = flags.subSequence(3, 4).equals("1")?true:false;
-//				nFlag = flags.subSequence(4, 5).equals("1")?true:false;
-//				carry = flags.subSequence(5, 6).equals("1")?true:false;
-//
-////				System.out.printf("%02X %02X %02X %s ",arg1,arg2,sum,flags);
-////				System.out.printf("  %s %s %s   %s %s %s %n", sign,zero,halfCarry,overflow,nFlag,carry);
-//						
-//				message = String.format("file INC -> %d  = %02X", arg1,sum);
-//				assertThat("ans: " + message,sum,equalTo(adder.increment(arg1)));
-//				assertThat("sign: " +  message,sign,equalTo(adder.hasSign()));
-//				assertThat("zero: " +  message,zero,equalTo(adder.isZero()));
-//				assertThat("halfCarry: " +  message,halfCarry,equalTo(adder.hasHalfCarry()));
-////				assertThat("overFlow: " +  message,overflow,equalTo(adder.hasOverflow()));
-//				assertThat("nFlag: " +  message,nFlag,equalTo(adder.isNFlagSet()));
-////				assertThat("carry: " +  message,carry,equalTo(adder.hasCarry()));			
-//			}//while
-//			scanner.close();
-//			inputStream.close();
-//		} catch (Exception e) {
-//			System.out.println(e.getMessage());
-//			fail(e.getMessage());
-//		}//try
-//	}//testINCfile
+			}//while
+			scanner.close();
+			inputStream.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			fail(e.getMessage());
+		}//try
+	}//testINCfile
 	
 	
 	
@@ -190,103 +194,5 @@ public class AdderByteAdd {
 		return(byte)tempInt;
 	}//getValue
 
-
-	
-	//--------------------------------------------------------------
-
-	@Test
-	public void testByteAdd() {
-		for (value1 = 0; value1 < 0XFF; value1++) {
-			for (value2 = 0; value2 < 0XFF; value2++) {
-				atu.fixFlagsADD(value1, value2, AdderTestUtility.BYTE_ARG, false);
-				byte answer = (byte) ((value1 + value2) & 0XFF);
-				bite1[0] = (byte) value1;
-				bite2[0] = (byte) value2;
-				aByte1 = (byte) value1;
-				aByte2 = (byte) value2;
-				answer =  (byte) (value1 + value2);
-
-//				assertThat("Full Add:  " + value1 + " " + value2, answer, equalTo(adder.add(bite1, bite2)));
-				assertThat("Full Add:  " + value1 + " " + value2, answer, equalTo(adder.add(aByte1, aByte2)));
-				assertThat("Full Add Sign:  " + value1 + " " + value2, atu.mSign, equalTo(adder.hasSign()));
-				assertThat("Full Add Zero:  " + value1 + " " + value2, atu.mZero, equalTo(adder.isZero()));
-				assertThat("Full Add HalfCarry:  " + value1 + " " + value2, atu.mHalfCarry, equalTo(adder.hasHalfCarry()));
-				assertThat("Full Add Parity:  " + value1 + " " + value2, atu.mParity, equalTo(adder.hasParity()));
-				assertThat("Full Add Overflow:  " + value1 + " " + value2, atu.mOverflow, equalTo(adder.hasOverflow()));
-				assertThat("Full Add nFlag:  " + value1 + " " + value2, atu.mNFlag, equalTo(adder.isNFlagSet()));
-				assertThat("Full Add Carry:  " + value1 + " " + value2, atu.mCarry, equalTo(adder.hasCarry()));
-			} // for val2
-		} // for val1
-	}// testByteAdd
-
-	@Test
-	public void testByteAddWithCarry() {
-		for (value1 = 0; value1 < 0XFF; value1++) {
-			for (value2 = 0; value2 < 0XFF; value2++) {
-				atu.fixFlagsADD(value1, value2, AdderTestUtility.BYTE_ARG, false);
-				byte answer = (byte) ((value1 + value2) & 0XFF);
-				bite1[0] = (byte) value1;
-				bite2[0] = (byte) value2;
-				
-				aByte1 = (byte) value1;
-				aByte2 = (byte) value2;
-				answer =  (byte) (value1 + value2);
-//				assertThat("Full Add/WC:  " + value1 + " " + value2, answer,
-//						equalTo(adder.addWithCarry(bite1, bite2, false)));
-				assertThat("Full Add/WC:  " + value1 + " " + value2, answer,
-						equalTo(adder.addWithCarry(aByte1, aByte2, false)));
-				assertThat("Full Add/WC Sign:  " + value1 + " " + value2, atu.mSign, equalTo(adder.hasSign()));
-				assertThat("Full Add/WC Zero:  " + value1 + " " + value2, atu.mZero, equalTo(adder.isZero()));
-				assertThat("Full Add/WC HalfCarry:  " + value1 + " " + value2, atu.mHalfCarry,
-						equalTo(adder.hasHalfCarry()));
-				assertThat("Full Add/WC Parity:  " + value1 + " " + value2, atu.mParity, equalTo(adder.hasParity()));
-				assertThat("Full Add/WC Overflow:  " + value1 + " " + value2, atu.mOverflow, equalTo(adder.hasOverflow()));
-				assertThat("Full Add/WC nFlag:  " + value1 + " " + value2, atu.mNFlag, equalTo(adder.isNFlagSet()));
-				assertThat("Full Add/WC Carry:  " + value1 + " " + value2, atu.mCarry, equalTo(adder.hasCarry()));
-
-				atu.fixFlagsADD(value1, value2, AdderTestUtility.BYTE_ARG, true);
-				answer = (byte) ((value1 + value2 + 1) & 0XFF);
-				bite1[0] = (byte) value1;
-				bite2[0] = (byte) value2;
-				aByte1 = (byte) value1;
-				aByte2 = (byte) value2;
-				answer =  (byte) (value1 + value2+1);
-//				assertThat("Full Add/WC:  " + value1 + " " + value2, answer,
-//						equalTo(adder.addWithCarry(bite1, bite2, true)));
-				assertThat("Full Add/WCt:  " + value1 + " " + value2, answer,
-						equalTo(adder.addWithCarry(aByte1, aByte2, true)));
-				assertThat("Full Add/WCt Sign:  " + value1 + " " + value2, atu.mSign, equalTo(adder.hasSign()));
-				assertThat("Full Add/WCt Zero:  " + value1 + " " + value2, atu.mZero, equalTo(adder.isZero()));
-				assertThat("Full Add/WCt HalfCarry:  " + value1 + " " + value2, atu.mHalfCarry,
-						equalTo(adder.hasHalfCarry()));
-				assertThat("Full Add/WCt Parity:  " + value1 + " " + value2, atu.mParity, equalTo(adder.hasParity()));
-				assertThat("Full Add/WCt Overflow:  " + value1 + " " + value2, atu.mOverflow, equalTo(adder.hasOverflow()));
-				assertThat("Full Add/WCt nFlag:  " + value1 + " " + value2, atu.mNFlag, equalTo(adder.isNFlagSet()));
-				assertThat("Full Add/WCt Carry:  " + value1 + " " + value2, atu.mCarry, equalTo(adder.hasCarry()));
-
-			} // for val2
-		} // for val1
-	}// testByteAddWithCarry
-
-
-	@Test
-	public void testByteIncrement() {
-		for (value1 = 0; value1 < 0XFF; value1++) {
-			atu.fixFlagsADD(value1, 1, AdderTestUtility.BYTE_ARG, false);
-				byte answer = (byte) ((value1 + 1) & 0XFF);
-				bite1[0] = (byte) value1;
-				
-				aByte1 = (byte) value1;
-				answer = (byte) (aByte1 +1);
-				assertThat("Full INC:  " + value1, answer, equalTo(adder.increment(aByte1)));
-				assertThat("Full INC Sign:  " + value1, atu.mSign, equalTo(adder.hasSign()));
-				assertThat("Full INC Zero:  " + value1, atu.mZero, equalTo(adder.isZero()));
-				assertThat("Full INC HalfCarry:  " + value1, atu.mHalfCarry, equalTo(adder.hasHalfCarry()));
-				assertThat("Full INC Parity:  " + value1, atu.mParity, equalTo(adder.hasParity()));
-				assertThat("Full INC Overflow:  " + value1, atu.mOverflow, equalTo(adder.hasOverflow()));
-				assertThat("Full INC nFlag:  " + value1, atu.mNFlag, equalTo(adder.isNFlagSet()));
-				assertThat("Full INC Carry:  " + value1, atu.mCarry, equalTo(adder.hasCarry()));
-		} // for val1
-	}// testByteIncrement
 
 }// class AdderTest2
--- VMZ80_Test/src/hardware/AdderWordSubtract.java
+++ VMZ80_Test/src/hardware/AdderWordSubtract.java
@@ -18,14 +18,16 @@ public class AdderWordSubtract {
 	boolean sign,zero,halfCarry,overflow,nFlag,carry;
 	boolean carryState;
 	String flags,message;
+	
+
 
 	
 	private static AdderTestUtility atu = AdderTestUtility.getInstance();
-	int value1, value2, answer;
-	byte[] bite1 = new byte[] { (byte) 0X00 };
-	byte[] bite2 = new byte[] { (byte) 0X00 };
-	byte[] word1 = new byte[] { (byte) 0X00, (byte) 0X00 };
-	byte[] word2 = new byte[] { (byte) 0X00, (byte) 0X00 };
+//	int value1, value2, answer;
+//	byte[] bite1 = new byte[] { (byte) 0X00 };
+//	byte[] bite2 = new byte[] { (byte) 0X00 };
+//	byte[] word1 = new byte[] { (byte) 0X00, (byte) 0X00 };
+//	byte[] word2 = new byte[] { (byte) 0X00, (byte) 0X00 };
 
 	@Before
 	public void setUp() throws Exception {
@@ -114,58 +116,30 @@ public class AdderWordSubtract {
 		return  new byte[] {lsb,msb};
 	}//getValue
 
-
-	
 	//---------------------------------------------------------------------
 
-//	@Test
-//	public void testWordSubWithCarry() {
-//		Random random = new Random();
-//		boolean carryState;
-//		int carryValue;
-//		int COUNT = 8000;
-//		for (int i = 0; i < COUNT; i++) {
-//			carryState = random.nextBoolean();
-//			carryValue = carryState?1:0;
-//			value1 = random.nextInt(0XFFFF);
-//			value2 = random.nextInt(0XFFFF);
-//			atu.fixFlagsSUB(value1, value2, AdderTestUtility.WORD_ARG, carryState);
-//			int answer = ((value1 - (value2 + carryValue)) & 0XFFFF);
-//			word1 = atu.loadWord(value1);
-//			word2 = atu.loadWord(value2);
-//			// byte[] result = adder.addWord(word1, word2);
-//			int ans = atu.getWordValue(adder.subWordWithCarry(word1, word2,carryState));
-//			assertThat("Full SubWord/WC:  " + value1 + " " + value2, answer, equalTo(ans));
-//			assertThat("Full SubWord/WC Sign:  " + value1 + " " + value2, atu.mSign, equalTo(adder.hasSign()));
-//			assertThat("Full SubWord/WC Zero:  " + value1 + " " + value2,atu. mZero, equalTo(adder.isZero()));
-//			assertThat("Full SubWord/WC HalfCarry:  " + value1 + " " + value2, atu. mHalfCarry, equalTo(adder.hasHalfCarry()));
-//			assertThat("Full SubWord/WC Parity:  " + value1 + " " + value2, atu. mParity, equalTo(adder.hasParity()));
-//			assertThat("Full SubWord/WC Overflow:  " + value1 + " " + value2, atu. mOverflow, equalTo(adder.hasOverflow()));
-//			assertThat("Full SubWord nFlag:  " + value1 + " " + value2, atu.mNFlag, equalTo(adder.isNFlagSet()));
-//			assertThat("Full SubWord/WC Carry:  " + value1 + " " + value2, atu. mCarry, equalTo(adder.hasCarry()));
-//
-//		} // for val1
-//	}// testWordSubWithCarry
-//	
-//	@Test
-//	public void testWordDecrement() {
-//		for (int value1 = 0; value1 <0XFFFF ; value1++) {
-//			
-//			atu.fixFlagsSUB(value1, 1, AdderTestUtility.WORD_ARG, false);
-//			int answer = (value1 -1) & 0XFFFF;
-//			word1 = atu.loadWord(value1);
-//			int ans = atu.getWordValue(adder.decrementWord(word1));
-//			assertThat("Full DecWord:  " + value1, answer, equalTo(ans));
-//			assertThat("Full DecWord Sign:  " + value1, atu.mSign, equalTo(adder.hasSign()));
-//			assertThat("Full DecWord Zero:  " + value1, atu.mZero, equalTo(adder.isZero()));
-//			assertThat("Full DecWord HalfCarry:  " + value1, atu.mHalfCarry, equalTo(adder.hasHalfCarry()));
-//			assertThat("Full DecWord Parity:  " + value1, atu.mParity, equalTo(adder.hasParity()));
-//			assertThat("Full DecWord Overflow:  " + value1, atu.mOverflow, equalTo(adder.hasOverflow()));
-//			assertThat("Full DecWord nFlag:  " + value1, atu.mNFlag, equalTo(adder.isNFlagSet()));
-//			assertThat("Full DecWord Carry:  " + value1, atu.mCarry, equalTo(adder.hasCarry()));
-//
-//		} // for val1
-//	}// testWordDecrement
+	@Test
+	public void testWordDecrement() {
+		// no flags are affected
+		byte[] word1;
+		for (int value1 = 0; value1 <0XFFFF ; value1++) {
+			
+			int answer = (value1 -1) & 0XFFFF;
+			word1 = loadWord(value1);
+			int ans = getWordValue(adder.decrementWord(word1));
+			assertThat("Full DecWord:  " + value1, answer, equalTo(ans));
+
+		} // for val1
+	}// testWordDecrement
+	
+	public byte[] loadWord(int value) {
+		return new byte[] { (byte) (value & 0XFF), (byte) ((value & 0XFF00) >> 8) };
+	}// loadWord
+	
+	public int getWordValue(byte[] word) {
+		return ((word[1] << 8) & 0XFF00) + (word[0] & 0X00FF);
+	}// getWordValue
+
 
 
 }// class AdderTest2
