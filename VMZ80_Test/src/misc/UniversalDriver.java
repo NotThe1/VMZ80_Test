d2a1844cd6f20033e1690cf9866e392af672b4da
Frank Martyn <FrankMartyn1945@gmail.com>
2018-03-08 14:25:26 -0500

    Set button one to DiskUtility

--- VMZ80_Test/src/misc/UniversalDriver.java
+++ VMZ80_Test/src/misc/UniversalDriver.java
@@ -37,8 +37,10 @@ import javax.swing.SwingConstants;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.SoftBevelBorder;
 
-import codeSupport.FilePicker;
+import disks.DiskDrive;
+import disks.DiskMetrics;
 import disks.utility.DiskUtility;
+import utilities.filePicker.FilePicker;
 
 public class UniversalDriver {
 
@@ -51,9 +53,8 @@ public class UniversalDriver {
 
 	private codeSupport.AppLogger log = codeSupport.AppLogger.getInstance();
 	private JTextPane txtLog;
-	private JPopupMenu popupLog; 
+	private JPopupMenu popupLog;
 	private AdapterLog logAdaper = new AdapterLog();
-
 
 	/**
 	 * Launch the application.
@@ -66,53 +67,60 @@ public class UniversalDriver {
 					window.frmTemplate.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
-				}//try
-			}//run
+				} // try
+			}// run
 		});
 	}// main
 
 	/* Standard Stuff */
-	
-	private void doBtnOne(){
-	}//doBtnOne
-	
-	private void doBtnTwo(){
-		
-	}//doBtnTwo
-	
-	private void doBtnThree(){
-		
-	}//doBtnThree
-	
-	private void doBtnFour(){
-		
-	}//doBtnFour
-	
-	//---------------------------------------------------------
-	
-	private void doFileNew(){
-		
-	}//doFileNew
-	private void doFileOpen(){
-		
-	}//doFileOpen
-	private void doFileSave(){
-		
-	}//doFileSave
-	private void doFileSaveAs(){
-		
-	}//doFileSaveAs
-	private void doFilePrint(){
-		
-	}//doFilePrint
-	private void doFileExit(){
+
+	private void doBtnOne() {
+		DiskUtility du = DiskUtility.getInstance();
+		du.setVisible(true);
+	}// doBtnOne
+
+	private void doBtnTwo() {
+
+	}// doBtnTwo
+
+	private void doBtnThree() {
+
+	}// doBtnThree
+
+	private void doBtnFour() {
+
+	}// doBtnFour
+
+	// ---------------------------------------------------------
+
+	private void doFileNew() {
+
+	}// doFileNew
+
+	private void doFileOpen() {
+
+	}// doFileOpen
+
+	private void doFileSave() {
+
+	}// doFileSave
+
+	private void doFileSaveAs() {
+
+	}// doFileSaveAs
+
+	private void doFilePrint() {
+
+	}// doFilePrint
+
+	private void doFileExit() {
 		appClose();
 		System.exit(0);
-	}//doFileExit
-
+	}// doFileExit
 
 	private void appClose() {
-		Preferences myPrefs =  Preferences.userNodeForPackage(UniversalDriver.class).node(this.getClass().getSimpleName());
+		Preferences myPrefs = Preferences.userNodeForPackage(UniversalDriver.class)
+				.node(this.getClass().getSimpleName());
 		Dimension dim = frmTemplate.getSize();
 		myPrefs.putInt("Height", dim.height);
 		myPrefs.putInt("Width", dim.width);
@@ -121,15 +129,16 @@ public class UniversalDriver {
 		myPrefs.putInt("LocY", point.y);
 		myPrefs.putInt("Divider", splitPane1.getDividerLocation());
 		myPrefs = null;
-	}//appClose
+	}// appClose
 
 	private void appInit() {
-		Preferences myPrefs =  Preferences.userNodeForPackage(UniversalDriver.class).node(this.getClass().getSimpleName());
+		Preferences myPrefs = Preferences.userNodeForPackage(UniversalDriver.class)
+				.node(this.getClass().getSimpleName());
 		frmTemplate.setSize(myPrefs.getInt("Width", 761), myPrefs.getInt("Height", 693));
 		frmTemplate.setLocation(myPrefs.getInt("LocX", 100), myPrefs.getInt("LocY", 100));
 		splitPane1.setDividerLocation(myPrefs.getInt("Divider", 250));
 		myPrefs = null;
-		
+
 		txtLog.setText(EMPTY_STRING);
 
 		log.setDoc(txtLog.getStyledDocument());
@@ -141,7 +150,7 @@ public class UniversalDriver {
 		initialize();
 		appInit();
 	}// Constructor
-	
+
 	private void doLogClear() {
 		log.clear();
 	}// doLogClear
@@ -163,8 +172,6 @@ public class UniversalDriver {
 
 	}// doLogPrint
 
-
-
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -185,7 +192,7 @@ public class UniversalDriver {
 		gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
 		gridBagLayout.rowWeights = new double[] { 0.0, 1.0, 0.0, Double.MIN_VALUE };
 		frmTemplate.getContentPane().setLayout(gridBagLayout);
-		
+
 		JPanel panelForButtons = new JPanel();
 		GridBagConstraints gbc_panelForButtons = new GridBagConstraints();
 		gbc_panelForButtons.anchor = GridBagConstraints.NORTH;
@@ -195,13 +202,13 @@ public class UniversalDriver {
 		gbc_panelForButtons.gridy = 0;
 		frmTemplate.getContentPane().add(panelForButtons, gbc_panelForButtons);
 		GridBagLayout gbl_panelForButtons = new GridBagLayout();
-		gbl_panelForButtons.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_panelForButtons.rowHeights = new int[]{0, 0};
-		gbl_panelForButtons.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panelForButtons.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panelForButtons.columnWidths = new int[] { 0, 0, 0, 0, 0, 0 };
+		gbl_panelForButtons.rowHeights = new int[] { 0, 0 };
+		gbl_panelForButtons.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panelForButtons.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
 		panelForButtons.setLayout(gbl_panelForButtons);
-		
-		btnOne = new JButton("Button 1");
+
+		btnOne = new JButton("Disk Utility");
 		btnOne.setMinimumSize(new Dimension(100, 20));
 		GridBagConstraints gbc_btnOne = new GridBagConstraints();
 		gbc_btnOne.insets = new Insets(0, 0, 0, 5);
@@ -217,7 +224,7 @@ public class UniversalDriver {
 		});
 		btnOne.setMaximumSize(new Dimension(0, 0));
 		btnOne.setPreferredSize(new Dimension(100, 20));
-		
+
 		btnTwo = new JButton("Button 2");
 		btnTwo.setMinimumSize(new Dimension(100, 20));
 		GridBagConstraints gbc_btnTwo = new GridBagConstraints();
@@ -233,7 +240,7 @@ public class UniversalDriver {
 		});
 		btnTwo.setPreferredSize(new Dimension(100, 20));
 		btnTwo.setMaximumSize(new Dimension(0, 0));
-		
+
 		btnThree = new JButton("Button 3");
 		btnThree.setMinimumSize(new Dimension(100, 20));
 		GridBagConstraints gbc_btnThree = new GridBagConstraints();
@@ -249,7 +256,7 @@ public class UniversalDriver {
 		});
 		btnThree.setPreferredSize(new Dimension(100, 20));
 		btnThree.setMaximumSize(new Dimension(0, 0));
-		
+
 		btnFour = new JButton("Button 4");
 		btnFour.setMinimumSize(new Dimension(100, 20));
 		GridBagConstraints gbc_btnFour = new GridBagConstraints();
@@ -264,7 +271,7 @@ public class UniversalDriver {
 		});
 		btnFour.setPreferredSize(new Dimension(100, 20));
 		btnFour.setMaximumSize(new Dimension(0, 0));
-		
+
 		splitPane1 = new JSplitPane();
 		GridBagConstraints gbc_splitPane1 = new GridBagConstraints();
 		gbc_splitPane1.insets = new Insets(0, 0, 5, 0);
@@ -272,36 +279,36 @@ public class UniversalDriver {
 		gbc_splitPane1.gridx = 0;
 		gbc_splitPane1.gridy = 1;
 		frmTemplate.getContentPane().add(splitPane1, gbc_splitPane1);
-		
+
 		JPanel panelLeft = new JPanel();
 		splitPane1.setLeftComponent(panelLeft);
 		GridBagLayout gbl_panelLeft = new GridBagLayout();
-		gbl_panelLeft.columnWidths = new int[]{0};
-		gbl_panelLeft.rowHeights = new int[]{0};
-		gbl_panelLeft.columnWeights = new double[]{Double.MIN_VALUE};
-		gbl_panelLeft.rowWeights = new double[]{Double.MIN_VALUE};
+		gbl_panelLeft.columnWidths = new int[] { 0 };
+		gbl_panelLeft.rowHeights = new int[] { 0 };
+		gbl_panelLeft.columnWeights = new double[] { Double.MIN_VALUE };
+		gbl_panelLeft.rowWeights = new double[] { Double.MIN_VALUE };
 		panelLeft.setLayout(gbl_panelLeft);
-		
+
 		JPanel panelForLog = new JPanel();
 		splitPane1.setRightComponent(panelForLog);
 		GridBagLayout gbl_panelForLog = new GridBagLayout();
-		gbl_panelForLog.columnWidths = new int[]{0, 0};
-		gbl_panelForLog.rowHeights = new int[]{0, 0};
-		gbl_panelForLog.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panelForLog.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panelForLog.columnWidths = new int[] { 0, 0 };
+		gbl_panelForLog.rowHeights = new int[] { 0, 0 };
+		gbl_panelForLog.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_panelForLog.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
 		panelForLog.setLayout(gbl_panelForLog);
-		
+
 		JScrollPane scrollPaneForLog = new JScrollPane();
 		GridBagConstraints gbc_scrollPaneForLog = new GridBagConstraints();
 		gbc_scrollPaneForLog.fill = GridBagConstraints.BOTH;
 		gbc_scrollPaneForLog.gridx = 0;
 		gbc_scrollPaneForLog.gridy = 0;
 		panelForLog.add(scrollPaneForLog, gbc_scrollPaneForLog);
-		
+
 		txtLog = new JTextPane();
 		scrollPaneForLog.setViewportView(txtLog);
-		
-		 popupLog = new JPopupMenu();
+
+		popupLog = new JPopupMenu();
 		addPopup(txtLog, popupLog);
 
 		JMenuItem popupLogClear = new JMenuItem("Clear Log");
@@ -317,15 +324,13 @@ public class UniversalDriver {
 		popupLogPrint.addActionListener(logAdaper);
 		popupLog.add(popupLogPrint);
 
-
-		
 		JLabel lblNewLabel = new JLabel("Application Log");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setForeground(new Color(0, 128, 0));
 		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 14));
 		scrollPaneForLog.setColumnHeaderView(lblNewLabel);
 		splitPane1.setDividerLocation(250);
-		
+
 		JPanel panelStatus = new JPanel();
 		panelStatus.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		GridBagConstraints gbc_panelStatus = new GridBagConstraints();
@@ -336,10 +341,10 @@ public class UniversalDriver {
 
 		JMenuBar menuBar = new JMenuBar();
 		frmTemplate.setJMenuBar(menuBar);
-		
+
 		JMenu mnuFile = new JMenu("File");
 		menuBar.add(mnuFile);
-		
+
 		JMenuItem mnuFileNew = new JMenuItem("New");
 		mnuFileNew.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -347,7 +352,7 @@ public class UniversalDriver {
 			}
 		});
 		mnuFile.add(mnuFileNew);
-		
+
 		JMenuItem mnuFileOpen = new JMenuItem("Open...");
 		mnuFileOpen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -355,10 +360,10 @@ public class UniversalDriver {
 			}
 		});
 		mnuFile.add(mnuFileOpen);
-		
+
 		JSeparator separator99 = new JSeparator();
 		mnuFile.add(separator99);
-		
+
 		JMenuItem mnuFileSave = new JMenuItem("Save...");
 		mnuFileSave.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -366,7 +371,7 @@ public class UniversalDriver {
 			}
 		});
 		mnuFile.add(mnuFileSave);
-		
+
 		JMenuItem mnuFileSaveAs = new JMenuItem("Save As...");
 		mnuFileSaveAs.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -374,10 +379,10 @@ public class UniversalDriver {
 			}
 		});
 		mnuFile.add(mnuFileSaveAs);
-		
+
 		JSeparator separator_2 = new JSeparator();
 		mnuFile.add(separator_2);
-		
+
 		JMenuItem mnuFilePrint = new JMenuItem("Print...");
 		mnuFilePrint.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -385,11 +390,10 @@ public class UniversalDriver {
 			}
 		});
 		mnuFile.add(mnuFilePrint);
-		
-		
+
 		JSeparator separator_1 = new JSeparator();
 		mnuFile.add(separator_1);
-		
+
 		JMenuItem mnuFileExit = new JMenuItem("Exit");
 		mnuFileExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -397,10 +401,10 @@ public class UniversalDriver {
 			}
 		});
 		mnuFile.add(mnuFileExit);
-		
+
 		JMenu mnuTargets = new JMenu("Targets");
 		menuBar.add(mnuTargets);
-		
+
 		JMenuItem mnuDiskUtility = new JMenuItem("DiskUtility");
 		mnuDiskUtility.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -408,39 +412,73 @@ public class UniversalDriver {
 				du.setVisible(true);
 			}
 		});
-		
+
 		JMenuItem mnuTargetsFileChooser = new JMenuItem("File Chooser");
 		mnuTargetsFileChooser.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-//				JFileChooser fc = FilePicker.customiseChooser("C:\\Temp\\", "Text", "txt");
-//				JFileChooser fc = FilePicker.getDiskPicker();
-//				JFileChooser fc = FilePicker.getMemoryPicker();
-//				JFileChooser fc = FilePicker.getListAsmPicker();
-//				JFileChooser fc = FilePicker.getListMemoryPicker();
-//				JFileChooser fc = FilePicker.getListZ80Picker();
-//				JFileChooser fc = FilePicker.getAllListPicker();
-//				JFileChooser fc = FilePicker.getZ80ListPicker();
+				// JFileChooser fc = FilePicker.customiseChooser("C:\\Temp\\", "Text", "txt");
+				// JFileChooser fc = FilePicker.getDiskPicker();
+				// JFileChooser fc = FilePicker.getMemoryPicker();
+				// JFileChooser fc = FilePicker.getListAsmPicker();
+				// JFileChooser fc = FilePicker.getListMemoryPicker();
+				// JFileChooser fc = FilePicker.getListZ80Picker();
+				// JFileChooser fc = FilePicker.getAllListPicker();
+				// JFileChooser fc = FilePicker.getZ80ListPicker();
 				JFileChooser fc = FilePicker.getZ80SourcePicker();
-//				JFileChooser fc = FilePicker
-//				JFileChooser fc = FilePicker
-//				JFileChooser fc = FilePicker
+				// JFileChooser fc = FilePicker
+				// JFileChooser fc = FilePicker
+				// JFileChooser fc = FilePicker
+
+				if (fc.showOpenDialog(frmTemplate)== JFileChooser.APPROVE_OPTION) {
+					log.addInfo(fc.getSelectedFile().toString());
+					log.addInfo(fc.getSelectedFile().getName());
+				};
 				
-				fc.showOpenDialog(frmTemplate);
 			}
 		});
 		mnuTargets.add(mnuTargetsFileChooser);
 		mnuTargets.add(mnuDiskUtility);
 
+		JMenuItem mnuTargetsDiskMetric = new JMenuItem("DiskMetric");
+		mnuTargetsDiskMetric.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String msg;
+				String[] types = new String[] { "f3DD", "f3HD", "f3ED", "f5DD", "f5HD", "f8SS","F8ds" };
+				for (String type : types) {
+					DiskMetrics diskMetric = DiskMetrics.getDiskMetric(type);
+					msg = String.format("Type %s, - %s", type, diskMetric.descriptor);
+					log.addInfo(msg);
+				} // for
+
+			}
+		});
+		mnuTargets.add(mnuTargetsDiskMetric);
 		
+		JMenuItem mnuTargetsDiskDrive = new JMenuItem("DiskDrive");
+		mnuTargetsDiskDrive.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String msg;
+				String[] types = new String[] { "f3DD", "f3HD", "f3ED", "f5DD", "f5HD", "f8SS","F8ds" };
+				for (String type : types) {
+					DiskDrive diskDrive = new DiskDrive("abc." + type);
+					msg = String.format("Type %s, - %s", type, diskDrive.description);
+					log.addInfo(msg);
+				} // for
+
+				
+			}
+		});
+		mnuTargets.add(mnuTargetsDiskDrive);
 
 	}// initialize
+
 	private static final String PUM_LOG_PRINT = "popupLogPrint";
 	private static final String PUM_LOG_CLEAR = "popupLogClear";
 
 	static final String EMPTY_STRING = "";
-	
+
 	//////////////////////////////////////////////////////////////////////////
-	
+
 	class AdapterLog implements ActionListener {// , ListSelectionListener
 		@Override
 		public void actionPerformed(ActionEvent actionEvent) {
