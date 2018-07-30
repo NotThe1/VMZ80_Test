3b72e287e0deb1865f6bf0d0d7a8f48478708d1f
Frank Martyn <FrankMartyn1945@gmail.com>
2018-07-30 14:45:31 -0400

    Added Program & Index Registers

--- VMZ80_Test/src/misc/ViewTests.java
+++ VMZ80_Test/src/misc/ViewTests.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -37,22 +38,19 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
 import javax.swing.border.BevelBorder;
-import javax.swing.border.SoftBevelBorder;
 
 import codeSupport.AppLogger;
 import codeSupport.Z80.Register;
 import hardware.ConditionCodeRegister;
 import hardware.WorkingRegisterSet;
+import hardware.View.V_CCR;
+import hardware.View.V_IndexRegisters;
 import hardware.View.V_PrimaryRegisters;
-import hardware.View.ViewCCR;
+import hardware.View.V_ProgramRegisters;
 
 public class ViewTests {
 
 	private JFrame frmTemplate;
-	private JButton btnOne;
-	private JButton btnTwo;
-	private JButton btnThree;
-	private JButton btnFour;
 	private JSplitPane splitPane1;
 
 	private AppLogger log = AppLogger.getInstance();
@@ -79,46 +77,6 @@ public class ViewTests {
 		});
 	}// main
 
-	/* Standard Stuff */
-
-	private void doBtnOne() {
-
-	}// doBtnOne
-
-	private void doBtnTwo() {
-
-	}// doBtnTwo
-
-	private void doBtnThree() {
-
-	}// doBtnThree
-
-	private void doBtnFour() {
-
-	}// doBtnFour
-
-	// ---------------------------------------------------------
-
-	private void doFileNew() {
-
-	}// doFileNew
-
-	private void doFileOpen() {
-
-	}// doFileOpen
-
-	private void doFileSave() {
-
-	}// doFileSave
-
-	private void doFileSaveAs() {
-
-	}// doFileSaveAs
-
-	private void doFilePrint() {
-
-	}// doFilePrint
-
 	private void doFileExit() {
 		appClose();
 		System.exit(0);
@@ -142,7 +100,7 @@ public class ViewTests {
 
 	private void appInit() {
 		Preferences myPrefs = Preferences.userNodeForPackage(ViewTests.class).node(this.getClass().getSimpleName());
-		frmTemplate.setSize(587, 539);
+		frmTemplate.setSize(1028, 539);
 		frmTemplate.setLocation(myPrefs.getInt("LocX", 100), myPrefs.getInt("LocY", 100));
 		splitPane1.setDividerLocation(myPrefs.getInt("Divider", 250));
 		myPrefs = null;
@@ -151,26 +109,36 @@ public class ViewTests {
 
 		log.setDoc(txtLog.getStyledDocument());
 		log.info("Starting....");
-		
-		wrs.setReg(Register.A, (byte)0X01);
-		wrs.setReg(Register.B, (byte)0X02);
-		wrs.setReg(Register.C, (byte)0X03);
-		wrs.setReg(Register.D, (byte)0X04);
-		wrs.setReg(Register.E, (byte)0X05);
-		wrs.setReg(Register.H, (byte)0X06);
-		wrs.setReg(Register.L, (byte)0X07);
-		wrs.setReg(Register.F, (byte)0X01);
 
-		wrs.setReg(Register.Ap, (byte)0X10);
-		wrs.setReg(Register.Bp, (byte)0X20);
-		wrs.setReg(Register.Cp, (byte)0X30);
-		wrs.setReg(Register.Dp, (byte)0X40);
-		wrs.setReg(Register.Ep, (byte)0X50);
-		wrs.setReg(Register.Hp, (byte)0X60);
-		wrs.setReg(Register.Lp, (byte)0X70);
-		wrs.setReg(Register.Fp, (byte)0XD7);
+		wrs.setReg(Register.A, (byte) 0X01);
+		wrs.setReg(Register.B, (byte) 0X02);
+		wrs.setReg(Register.C, (byte) 0X03);
+		wrs.setReg(Register.D, (byte) 0X04);
+		wrs.setReg(Register.E, (byte) 0X05);
+		wrs.setReg(Register.H, (byte) 0X06);
+		wrs.setReg(Register.L, (byte) 0X07);
+		wrs.setReg(Register.F, (byte) 0X01);
+
+		wrs.setReg(Register.Ap, (byte) 0X10);
+		wrs.setReg(Register.Bp, (byte) 0X20);
+		wrs.setReg(Register.Cp, (byte) 0X30);
+		wrs.setReg(Register.Dp, (byte) 0X40);
+		wrs.setReg(Register.Ep, (byte) 0X50);
+		wrs.setReg(Register.Hp, (byte) 0X60);
+		wrs.setReg(Register.Lp, (byte) 0X70);
+		wrs.setReg(Register.Fp, (byte) 0XD7);
 		Thread t_PrimayRegisters = new Thread(panel_V_PrimayRegisters);
 		t_PrimayRegisters.run();
+
+		wrs.setProgramCounter(0x0100);
+		wrs.setStackPointer(0x1111);
+		Thread t_ProgramRegisters = new Thread(panel_V_ProgramRegisters);
+		t_ProgramRegisters.run();
+
+		wrs.setIX(0xA5A5);
+		wrs.setIY(0x5A5A);
+		Thread t_IndexRegisters = new Thread(panel_V_IndexRegisters);
+		t_IndexRegisters.run();
 
 	}// appInit
 
@@ -236,69 +204,20 @@ public class ViewTests {
 		gbl_panelForButtons.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
 		panelForButtons.setLayout(gbl_panelForButtons);
 
-		btnOne = new JButton("Button 1");
-		btnOne.setMinimumSize(new Dimension(100, 20));
-		GridBagConstraints gbc_btnOne = new GridBagConstraints();
-		gbc_btnOne.insets = new Insets(0, 0, 0, 5);
-		gbc_btnOne.gridx = 0;
-		gbc_btnOne.gridy = 0;
-		panelForButtons.add(btnOne, gbc_btnOne);
-		btnOne.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		btnOne.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		btnOne.addActionListener(new ActionListener() {
+		JButton btnNewButton_1 = new JButton("test Registers");
+		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				doBtnOne();
-			}
+				Thread t_ProgramRegisters = new Thread(panel_V_ProgramRegisters);
+				t_ProgramRegisters.run();
+				Thread t_IndexRegisters = new Thread(panel_V_IndexRegisters);
+				t_IndexRegisters.run();
+			}//actionPerformed
 		});
-		btnOne.setMaximumSize(new Dimension(0, 0));
-		btnOne.setPreferredSize(new Dimension(100, 20));
-
-		btnTwo = new JButton("Button 2");
-		btnTwo.setMinimumSize(new Dimension(100, 20));
-		GridBagConstraints gbc_btnTwo = new GridBagConstraints();
-		gbc_btnTwo.insets = new Insets(0, 0, 0, 5);
-		gbc_btnTwo.gridx = 1;
-		gbc_btnTwo.gridy = 0;
-		panelForButtons.add(btnTwo, gbc_btnTwo);
-		btnTwo.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		btnTwo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				doBtnTwo();
-			}
-		});
-		btnTwo.setPreferredSize(new Dimension(100, 20));
-		btnTwo.setMaximumSize(new Dimension(0, 0));
-
-		btnThree = new JButton("Button 3");
-		btnThree.setMinimumSize(new Dimension(100, 20));
-		GridBagConstraints gbc_btnThree = new GridBagConstraints();
-		gbc_btnThree.insets = new Insets(0, 0, 0, 5);
-		gbc_btnThree.gridx = 3;
-		gbc_btnThree.gridy = 0;
-		panelForButtons.add(btnThree, gbc_btnThree);
-		btnThree.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		btnThree.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				doBtnThree();
-			}
-		});
-		btnThree.setPreferredSize(new Dimension(100, 20));
-		btnThree.setMaximumSize(new Dimension(0, 0));
-
-		btnFour = new JButton("Button 4");
-		btnFour.setMinimumSize(new Dimension(100, 20));
-		GridBagConstraints gbc_btnFour = new GridBagConstraints();
-		gbc_btnFour.gridx = 4;
-		gbc_btnFour.gridy = 0;
-		panelForButtons.add(btnFour, gbc_btnFour);
-		btnFour.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		btnFour.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				doBtnFour();
-			}
-		});
-		btnFour.setPreferredSize(new Dimension(100, 20));
-		btnFour.setMaximumSize(new Dimension(0, 0));
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton_1.gridx = 0;
+		gbc_btnNewButton_1.gridy = 0;
+		panelForButtons.add(btnNewButton_1, gbc_btnNewButton_1);
 
 		splitPane1 = new JSplitPane();
 		GridBagConstraints gbc_splitPane1 = new GridBagConstraints();
@@ -311,7 +230,7 @@ public class ViewTests {
 		JPanel panelLeft = new JPanel();
 		splitPane1.setLeftComponent(panelLeft);
 		GridBagLayout gbl_panelLeft = new GridBagLayout();
-		gbl_panelLeft.columnWidths = new int[] { 0, 0 };
+		gbl_panelLeft.columnWidths = new int[] { 100, 0 };
 		gbl_panelLeft.rowHeights = new int[] { 0, 0 };
 		gbl_panelLeft.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
 		gbl_panelLeft.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
@@ -330,50 +249,71 @@ public class ViewTests {
 		GridBagLayout gbl_panelPrimaryRegisters = new GridBagLayout();
 		gbl_panelPrimaryRegisters.columnWidths = new int[] { 200, 0 };
 		gbl_panelPrimaryRegisters.rowHeights = new int[] { 0, 0, 0 };
-		gbl_panelPrimaryRegisters.columnWeights = new double[] { 0.0, Double.MIN_VALUE };
-		gbl_panelPrimaryRegisters.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panelPrimaryRegisters.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_panelPrimaryRegisters.rowWeights = new double[] { 0.0, 0.0, 0.0 };
 		panelPrimaryRegisters.setLayout(gbl_panelPrimaryRegisters);
 
+		V_CCR viewCCR = new V_CCR();
+		viewCCR.setSize(new Dimension(0, 0));
+		viewCCR.setMinimumSize(new Dimension(190, 54));
+		GridBagConstraints gbc_viewCCR = new GridBagConstraints();
+		gbc_viewCCR.anchor = GridBagConstraints.NORTHWEST;
+		gbc_viewCCR.insets = new Insets(0, 0, 5, 0);
+		gbc_viewCCR.gridx = 0;
+		gbc_viewCCR.gridy = 0;
+		panelPrimaryRegisters.add(viewCCR, gbc_viewCCR);
+		viewCCR.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
 		panel_V_PrimayRegisters = new V_PrimaryRegisters();
-		panel_V_PrimayRegisters.setMaximumSize(new Dimension(0, 0));
+		panel_V_PrimayRegisters.setPreferredSize(new Dimension(450, 90));
+		panel_V_PrimayRegisters.setLayout(null);
+
 		GridBagConstraints gbc_panel_V_PrimayRegisters = new GridBagConstraints();
-		gbc_panel_V_PrimayRegisters.anchor = GridBagConstraints.WEST;
-		gbc_panel_V_PrimayRegisters.fill = GridBagConstraints.VERTICAL;
+		gbc_panel_V_PrimayRegisters.anchor = GridBagConstraints.NORTHWEST;
 		gbc_panel_V_PrimayRegisters.gridx = 0;
 		gbc_panel_V_PrimayRegisters.gridy = 1;
 		panelPrimaryRegisters.add(panel_V_PrimayRegisters, gbc_panel_V_PrimayRegisters);
-		GridBagLayout gbl_panel_V_PrimayRegisters = new GridBagLayout();
-		gbl_panel_V_PrimayRegisters.columnWidths = new int[]{0};
-		gbl_panel_V_PrimayRegisters.rowHeights = new int[]{0};
-		gbl_panel_V_PrimayRegisters.columnWeights = new double[]{Double.MIN_VALUE};
-		gbl_panel_V_PrimayRegisters.rowWeights = new double[]{Double.MIN_VALUE};
-		panel_V_PrimayRegisters.setLayout(gbl_panel_V_PrimayRegisters);
+
+		panel_V_ProgramRegisters = new V_ProgramRegisters();
+		panel_V_ProgramRegisters.setPreferredSize(new Dimension(240, 75));
+		panel_V_ProgramRegisters.setLayout(null);
+
+		GridBagConstraints gbc_panel_V_ProgramRegisters = new GridBagConstraints();
+		gbc_panel_V_ProgramRegisters.anchor = GridBagConstraints.NORTHWEST;
+		gbc_panel_V_ProgramRegisters.gridx = 0;
+		gbc_panel_V_ProgramRegisters.gridy = 2;
+		panelPrimaryRegisters.add(panel_V_ProgramRegisters, gbc_panel_V_ProgramRegisters);
+
+		//
+		panel_V_IndexRegisters = new V_IndexRegisters();
+		panel_V_IndexRegisters.setPreferredSize(new Dimension(240, 75));
+		panel_V_IndexRegisters.setLayout(null);
+
+		GridBagConstraints gbc_panel_V_IndexRegisters = new GridBagConstraints();
+		gbc_panel_V_IndexRegisters.anchor = GridBagConstraints.NORTHWEST;
+		gbc_panel_V_IndexRegisters.gridx = 0;
+		gbc_panel_V_IndexRegisters.gridy = 3;
+		panelPrimaryRegisters.add(panel_V_IndexRegisters, gbc_panel_V_IndexRegisters);
+		//
 
 		JPanel panelCCR = new JPanel();
 		tabbedPane.addTab("CCR", null, panelCCR, null);
 		GridBagLayout gbl_panelCCR = new GridBagLayout();
-		gbl_panelCCR.columnWidths = new int[] { 0, 214, 0 };
+		gbl_panelCCR.columnWidths = new int[] { 200 };
 		gbl_panelCCR.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		gbl_panelCCR.columnWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panelCCR.columnWeights = new double[] { 1.0 };
 		gbl_panelCCR.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
 				Double.MIN_VALUE };
 		panelCCR.setLayout(gbl_panelCCR);
 
-		Component horizontalStrut = Box.createHorizontalStrut(20);
-		GridBagConstraints gbc_horizontalStrut = new GridBagConstraints();
-		gbc_horizontalStrut.insets = new Insets(0, 0, 5, 5);
-		gbc_horizontalStrut.gridx = 0;
-		gbc_horizontalStrut.gridy = 0;
-		panelCCR.add(horizontalStrut, gbc_horizontalStrut);
-
-		ViewCCR panelViewCCR = new ViewCCR();
-		panelViewCCR.setSize(new Dimension(0, 0));
-		panelViewCCR.setMinimumSize(new Dimension(190, 54));
+		V_CCR panelViewCCR = new V_CCR();
+		// panelViewCCR.setSize(new Dimension(0, 0));
+		panelViewCCR.setMinimumSize(new Dimension(10, 10));
 		GridBagConstraints gbc_panelViewCCR = new GridBagConstraints();
 		gbc_panelViewCCR.anchor = GridBagConstraints.WEST;
 		gbc_panelViewCCR.insets = new Insets(0, 0, 5, 0);
 		gbc_panelViewCCR.fill = GridBagConstraints.VERTICAL;
-		gbc_panelViewCCR.gridx = 1;
+		gbc_panelViewCCR.gridx = 0;
 		gbc_panelViewCCR.gridy = 1;
 		panelCCR.add(panelViewCCR, gbc_panelViewCCR);
 		GridBagLayout gbl_panelViewCCR = new GridBagLayout();
@@ -382,11 +322,12 @@ public class ViewTests {
 		gbl_panelViewCCR.columnWeights = new double[] { Double.MIN_VALUE };
 		gbl_panelViewCCR.rowWeights = new double[] { Double.MIN_VALUE };
 		panelViewCCR.setLayout(gbl_panelViewCCR);
-
+		//
 		Component verticalStrut = Box.createVerticalStrut(20);
 		GridBagConstraints gbc_verticalStrut = new GridBagConstraints();
+		gbc_verticalStrut.anchor = GridBagConstraints.WEST;
 		gbc_verticalStrut.insets = new Insets(0, 0, 5, 0);
-		gbc_verticalStrut.gridx = 1;
+		gbc_verticalStrut.gridx = 0;
 		gbc_verticalStrut.gridy = 2;
 		panelCCR.add(verticalStrut, gbc_verticalStrut);
 
@@ -394,7 +335,7 @@ public class ViewTests {
 		GridBagConstraints gbc_cbSign = new GridBagConstraints();
 		gbc_cbSign.insets = new Insets(0, 0, 5, 0);
 		gbc_cbSign.anchor = GridBagConstraints.WEST;
-		gbc_cbSign.gridx = 1;
+		gbc_cbSign.gridx = 0;
 		gbc_cbSign.gridy = 3;
 		panelCCR.add(cbSign, gbc_cbSign);
 
@@ -402,7 +343,7 @@ public class ViewTests {
 		GridBagConstraints gbc_cbZero = new GridBagConstraints();
 		gbc_cbZero.anchor = GridBagConstraints.WEST;
 		gbc_cbZero.insets = new Insets(0, 0, 5, 0);
-		gbc_cbZero.gridx = 1;
+		gbc_cbZero.gridx = 0;
 		gbc_cbZero.gridy = 4;
 		panelCCR.add(cbZero, gbc_cbZero);
 
@@ -410,7 +351,7 @@ public class ViewTests {
 		GridBagConstraints gbc_cbHalfCarry = new GridBagConstraints();
 		gbc_cbHalfCarry.anchor = GridBagConstraints.WEST;
 		gbc_cbHalfCarry.insets = new Insets(0, 0, 5, 0);
-		gbc_cbHalfCarry.gridx = 1;
+		gbc_cbHalfCarry.gridx = 0;
 		gbc_cbHalfCarry.gridy = 5;
 		panelCCR.add(cbHalfCarry, gbc_cbHalfCarry);
 
@@ -418,7 +359,7 @@ public class ViewTests {
 		GridBagConstraints gbc_cbParity = new GridBagConstraints();
 		gbc_cbParity.anchor = GridBagConstraints.WEST;
 		gbc_cbParity.insets = new Insets(0, 0, 5, 0);
-		gbc_cbParity.gridx = 1;
+		gbc_cbParity.gridx = 0;
 		gbc_cbParity.gridy = 6;
 		panelCCR.add(cbParity, gbc_cbParity);
 
@@ -426,7 +367,7 @@ public class ViewTests {
 		GridBagConstraints gbc_cbNFlag = new GridBagConstraints();
 		gbc_cbNFlag.anchor = GridBagConstraints.WEST;
 		gbc_cbNFlag.insets = new Insets(0, 0, 5, 0);
-		gbc_cbNFlag.gridx = 1;
+		gbc_cbNFlag.gridx = 0;
 		gbc_cbNFlag.gridy = 7;
 		panelCCR.add(cbNFlag, gbc_cbNFlag);
 
@@ -434,14 +375,15 @@ public class ViewTests {
 		GridBagConstraints gbc_cbCarry = new GridBagConstraints();
 		gbc_cbCarry.anchor = GridBagConstraints.WEST;
 		gbc_cbCarry.insets = new Insets(0, 0, 5, 0);
-		gbc_cbCarry.gridx = 1;
+		gbc_cbCarry.gridx = 0;
 		gbc_cbCarry.gridy = 8;
 		panelCCR.add(cbCarry, gbc_cbCarry);
 
 		Component verticalStrut_1 = Box.createVerticalStrut(20);
 		GridBagConstraints gbc_verticalStrut_1 = new GridBagConstraints();
+		gbc_verticalStrut_1.anchor = GridBagConstraints.WEST;
 		gbc_verticalStrut_1.insets = new Insets(0, 0, 5, 0);
-		gbc_verticalStrut_1.gridx = 1;
+		gbc_verticalStrut_1.gridx = 0;
 		gbc_verticalStrut_1.gridy = 10;
 		panelCCR.add(verticalStrut_1, gbc_verticalStrut_1);
 
@@ -460,7 +402,7 @@ public class ViewTests {
 		});
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
 		gbc_btnNewButton.anchor = GridBagConstraints.WEST;
-		gbc_btnNewButton.gridx = 1;
+		gbc_btnNewButton.gridx = 0;
 		gbc_btnNewButton.gridy = 11;
 		panelCCR.add(btnNewButton, gbc_btnNewButton);
 
@@ -504,7 +446,7 @@ public class ViewTests {
 		lblNewLabel.setForeground(new Color(0, 128, 0));
 		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 14));
 		scrollPaneForLog.setColumnHeaderView(lblNewLabel);
-		splitPane1.setDividerLocation(250);
+		splitPane1.setDividerLocation(200);
 
 		JPanel panelStatus = new JPanel();
 		panelStatus.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
@@ -520,54 +462,8 @@ public class ViewTests {
 		JMenu mnuFile = new JMenu("File");
 		menuBar.add(mnuFile);
 
-		JMenuItem mnuFileNew = new JMenuItem("New");
-		mnuFileNew.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				doFileNew();
-			}
-		});
-		mnuFile.add(mnuFileNew);
-
-		JMenuItem mnuFileOpen = new JMenuItem("Open...");
-		mnuFileOpen.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				doFileOpen();
-			}
-		});
-		mnuFile.add(mnuFileOpen);
-
 		JSeparator separator99 = new JSeparator();
 		mnuFile.add(separator99);
-
-		JMenuItem mnuFileSave = new JMenuItem("Save...");
-		mnuFileSave.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				doFileSave();
-			}
-		});
-		mnuFile.add(mnuFileSave);
-
-		JMenuItem mnuFileSaveAs = new JMenuItem("Save As...");
-		mnuFileSaveAs.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				doFileSaveAs();
-			}
-		});
-		mnuFile.add(mnuFileSaveAs);
-
-		JSeparator separator_2 = new JSeparator();
-		mnuFile.add(separator_2);
-
-		JMenuItem mnuFilePrint = new JMenuItem("Print...");
-		mnuFilePrint.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				doFilePrint();
-			}
-		});
-		mnuFile.add(mnuFilePrint);
-
-		JSeparator separator_1 = new JSeparator();
-		mnuFile.add(separator_1);
 
 		JMenuItem mnuFileExit = new JMenuItem("Exit");
 		mnuFileExit.addActionListener(new ActionListener() {
@@ -578,11 +474,6 @@ public class ViewTests {
 		mnuFile.add(mnuFileExit);
 
 	}// initialize
-
-	private static final String MAIN = "Main";
-	private static final String AUXILARY = "Auxilary";
-	private static final String[] REGS_MAIN = new String[] { "A", "B", "C", "D", "E", "H", "L", "F" };
-	private static final String[] REGS_AUX = new String[] { "A'", "B'", "C'", "D'", "E'", "H'", "L'", "F'" };
 
 	private static final String PUM_LOG_PRINT = "popupLogPrint";
 	private static final String PUM_LOG_CLEAR = "popupLogClear";
@@ -595,7 +486,8 @@ public class ViewTests {
 	private JCheckBox cbNFlag;
 	private JCheckBox cbCarry;
 	private V_PrimaryRegisters panel_V_PrimayRegisters;
-
+	private V_ProgramRegisters panel_V_ProgramRegisters;
+	private V_IndexRegisters panel_V_IndexRegisters;
 	//////////////////////////////////////////////////////////////////////////
 
 	class AdapterLog implements ActionListener {// , ListSelectionListener
@@ -632,5 +524,4 @@ public class ViewTests {
 			}
 		});
 	}// addPopup
-
 }// class GUItemplate
